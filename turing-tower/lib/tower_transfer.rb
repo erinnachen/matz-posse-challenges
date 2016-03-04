80d46d24dfb78261517730a18fe6fdd0625ec23a
Erinna Chen <erinna.chen@gmail.com>
2016-03-04 08:09:02 -0700

    Sweet sweet recursive tower

--- turing-tower/lib/tower_transfer.rb
+++ turing-tower/lib/tower_transfer.rb
@@ -1,57 +1,50 @@
 class TowerTransfer
-  attr_reader :posts
-  def initialize(n = 5)
+  attr_reader :posts, :print_flag
+  def initialize(n = 5, print_flag = false)
     @posts = {}
     @posts[1] = (1..n).to_a.reverse
     @posts[2] = []
     @posts[3] = []
+    @print_flag = print_flag
   end
 
   def move_tower
     transfer(1, 3, @posts[1].length)
   end
 
-  def transfer(source_post_ind,target_post_ind, n)
+  private
+
+  def transfer(source_post_ind, target_post_ind, n)
     source_post = posts[source_post_ind]
     target_post = posts[target_post_ind]
-    puts "CALLED TRANSFER WITH SOURCE: #{source_post_ind}, TARGET: #{target_post_ind} and NDISCS: #{n}"
+    puts "CALLED TRANSFER WITH SOURCE: #{source_post_ind}, TARGET: #{target_post_ind} and NDISCS: #{n}" if print_flag
     if n == 1
       target_post << source_post.pop
-      print_posts
+      print_posts if print_flag
       1
     else
-      transfer(source_post_ind, empty_post_ind(source_post_ind, target_post_ind), n-1)+transfer(source_post_ind, target_post_ind, 1) +
-      transfer(empty_post_ind(source_post_ind, target_post_ind), target_post_ind, n-1)
+      empty_post_ind = empty_post_index(source_post_ind, target_post_ind)
+      # Move stack of size n-1 to the empty post, move the biggest disc to the target post and move stack of size n-1 to the target post
+      transfer(source_post_ind, empty_post_ind, n-1) +
+      transfer(source_post_ind, target_post_ind, 1) +
+      transfer(empty_post_ind, target_post_ind, n-1)
     end
   end
 
-  def empty_post_ind(source_post_ind, target_post_ind)
-    if source_post_ind==1 && target_post_ind==3
-      2
-    elsif source_post_ind==1&& target_post_ind==2
-      3
-    elsif source_post_ind==2&& target_post_ind==1
-      3
-    elsif source_post_ind==2&& target_post_ind==3
-      1
-    elsif source_post_ind==3&& target_post_ind==1
-      2
-    else
-      1
-    end
+  def empty_post_index(source_post_ind, target_post_ind)
+    6 - source_post_ind - target_post_ind
   end
 
   def print_posts
-    puts "STATUS: 1: #{@posts[1].inspect}\n
-    2: #{@posts[2].inspect}\n
-    3: #{@posts[3].inspect}"
+    puts "STATUS: 1: #{posts[1].inspect}\n
+    2: #{posts[2].inspect}\n
+    3: #{posts[3].inspect}"
   end
 
 end
 
 if __FILE__ == $0
-  n = 10
-  tw = TowerTransfer.new(n)
+  n = ARGV[0].to_i || 4
+  tw = TowerTransfer.new(n, true)
   puts "TOTAL number of moves: #{tw.move_tower}"
-  puts "Formula = #{2**n-1}"
 end
--- turing-tower/test/tower_transfer_test.rb
+++ turing-tower/test/tower_transfer_test.rb
@@ -6,23 +6,35 @@ class TowerTransferTest < Minitest::Test
     tower = TowerTransfer.new(1)
     moves = tower.move_tower
     assert_equal 1, moves
+    assert_equal [], tower.posts[1]
+    assert_equal [], tower.posts[2]
+    assert_equal [1], tower.posts[3]
   end
 
   def test_tower_transfers_two_discs_in_three_moves
     tower = TowerTransfer.new(2)
     moves = tower.move_tower
     assert_equal 3, moves
+    assert_equal [], tower.posts[1]
+    assert_equal [], tower.posts[2]
+    assert_equal [2,1], tower.posts[3]
   end
 
   def test_tower_transfers_three_discs_in_seven_moves
     tower = TowerTransfer.new(3)
     moves = tower.move_tower
     assert_equal 7, moves
+    assert_equal [], tower.posts[1]
+    assert_equal [], tower.posts[2]
+    assert_equal [3,2,1], tower.posts[3]
   end
 
   def test_tower_transfers_n_discs_in_2_to_the_n_minus_1_moves
     tower = TowerTransfer.new(8)
     moves = tower.move_tower
     assert_equal 255, moves
+    assert_equal [], tower.posts[1]
+    assert_equal [], tower.posts[2]
+    assert_equal [8, 7, 6, 5, 4, 3, 2, 1], tower.posts[3]
   end
 end
